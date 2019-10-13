70e8c95926b53c97e89eefb04087124cc27e6848
Hersh Bhargava <hershkbhargava@gmail.com>
2019-10-12 23:48:21 -0700

    case stuff

--- src/core/textinput.cpp
+++ src/core/textinput.cpp
@@ -1,10 +1,10 @@
 #include "core_headers.h"
-#include <nlohmann/json.hpp>
+// #include <nlohmann/json.hpp>
 
-using json = nlohmann::json;
+// using json = nlohmann::json;
 
 TextInput::TextInput(const char * filePath)
 {
-	path = filePath;
-
+	// path = filePath;
+	wxPrintf("Initialized textinput");
 }
