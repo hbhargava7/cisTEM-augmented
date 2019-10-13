3d78fd56e9ad7aec4ea0812850b35914aad495f1
Hersh Bhargava <hershkbhargava@gmail.com>
2019-10-12 22:46:23 -0700

    ibid

--- src/core/textInput.h
+++ src/core/textInput.h
@@ -4,7 +4,7 @@ class TextInput {
 		char path[1000];
 
 	public:
-		void TextInput(wxString * filePath);
+		TextInput(wxString * filePath);
 		float GetFloatFromFile(const char * filePath);
 		int GetIntFromFile(const char * filePath);
 		std::string GetStringFromFile(const char * filePath);
