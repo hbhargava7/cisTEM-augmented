58a59a636c8bf85aa48f8bd1d6866fdaff1b0e5f
Hersh Bhargava <hershkbhargava@gmail.com>
2019-10-13 12:16:18 -0700

    ibid

--- src/programs/refine3d/refine3d.cpp
+++ src/programs/refine3d/refine3d.cpp
@@ -166,7 +166,7 @@ void Refine3DApp::DoInteractiveUserInput()
 	std::string inputPath = std::string(wxAppConsole::argv[1]);
 	TextInput *input = new TextInput(inputPath);
 
-	std::string *word = std::string("chicken");
+	std::string word = "chicken";
 	input->GetLineWithFirstWord(word);
 	// input->GetLineWithFirstWord(patch::to_string("Chicken"));
 
