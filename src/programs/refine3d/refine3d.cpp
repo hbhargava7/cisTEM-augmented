6dff4616ffec3bffcd4386873f560f36ecdc8a86
Hersh Bhargava <hershkbhargava@gmail.com>
2019-10-12 22:12:57 -0700

    ibid

--- src/programs/refine3d/refine3d.cpp
+++ src/programs/refine3d/refine3d.cpp
@@ -137,7 +137,7 @@ void Refine3DApp::DoInteractiveUserInput()
 	bool		normalize_input_3d = true;
 	bool		threshold_input_3d = true;
 
-	wxPrintf(wxAppConsole::argv);
+	wxPrintf(wxString::Format("%s", wxAppConsole::argv));
 	
 	UserInput *my_input = new UserInput("Hersh's Augmented Refine3D", 1.02);
 
