ac17735ab9fa2c5ba680cfcf2ea0fe402fd6d840
Hersh Bhargava <hershkbhargava@gmail.com>
2019-10-13 12:21:17 -0700

    ibid

--- src/programs/refine3d/refine3d.cpp
+++ src/programs/refine3d/refine3d.cpp
@@ -60,12 +60,12 @@ TextInput::TextInput(std::string filePath)
 }
 std::string TextInput::GetLineWithFirstWord(std::string firstWord)
 {
-	wxPrintf(wxString::ToStdString(firstWord));
+	wxPrintf(std::string("Searching for word: ") + firstWord);
 	std::ifstream file(path.c_str());
     std::string str; 
     while (std::getline(file, str))
     {
-        wxPrintf("Current Line: %s", str);
+        wxPrintf(std::string("Current Line: ") + str);
     }
 }
 
